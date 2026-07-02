7fdb32c4eb0d941fcbd22b3673219593b48750d4
zyadhossam172-hub <zyadhossam172@gmail.com>
2026-07-02 14:02:17 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import telebot
 
+BOT_TOKEN = "8350793522:AAHbEDNEFruQFiNm-7S73pioYmWxYTZSbQw"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "أهلاً بك يا زياد! بوت التليجرام بتاعك شغال بنجاح فوراً 🚀")
+
+print("البوت مستعد وشغال...")
+bot.infinity_polling()
