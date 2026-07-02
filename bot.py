073c78dc21e1c88dd3ccf70762ddb1f696a36d2a
zyadhossam172-hub <zyadhossam172@gmail.com>
2026-07-02 14:09:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import telebot
 
-BOT_TOKEN = "8350793522:AAHbEDNEFruQFiNm-7S73pioYmWxYTZSbQw"
+BOT_TOKEN = "اكتب_التوكن_بتاعك_هنا"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك يا زياد! بوت التليجرام بتاعك شغال بنجاح فوراً 🚀")
+    user_name = message.from_user.first_name
+    bot.reply_to(message, f"أهلاً بك يا {user_name}! البوت شغال أونلاين على السيرفر بنجاح 🚀")
 
-print("البوت مستعد وشغال...")
+print("البوت شغال...")
 bot.infinity_polling()
